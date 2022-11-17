c2fcdf4dda91d0a981c75dbed8f50f07e43aefaa
Shyamal Kirpan <shyamalkirpan20@gmail.com>
2022-11-17 14:14:36 +0530

    feat: :fire: add pocketbase module

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,12 +1,26 @@
+import Pocketbase from "pocketbase";
 import Link from "next/link";
 import styles from "./Notes.module.css";
 
+export const dynamic = "auto",
+  dynamicParams = true,
+  revalidate = 0,
+  fetchCache = "auto",
+  runtime = "nodejs",
+  preferredRegion = "auto";
+
 async function getnotes() {
-  const res = await fetch(
-    "http://127.0.0.1:8090/api/collections/notes/records?page=1&perPage=30",
-    { cache: "no-store" }
-  );
-  const data = await res.json();
+  // using Fetch API
+  // const res = await fetch(
+  //   "http://127.0.0.1:8090/api/collections/notes/records?page=1&perPage=30",
+  //   { cache: "no-store" }
+  // );
+  // const data = await res.json();
+
+  // using pocketbase
+  const db = new Pocketbase("http://127.0.0.1:8090");
+  const data = await db.records.getList("notes");
+
   return data?.items as any[];
 }
 
